b1d5d4db022a28d00634813ab5607da69880fc95
sandeepkumaryadav05 <sandeepyadav051003@gmail.com>
2025-06-17 10:31:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Sandy.051003",
+  password: "xxxx",
   database: "quizDB"
 });
 
